0e09d92751d8422dbbf4b1d3c70edb3f356eb120
dobby-zent <dobby.lee@zent.kr>
2024-04-05 17:07:32 +0900

    Add to recurAddValue

--- service.ts
+++ service.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-  return res.status(200).send("hello world");
+  var result: number = 0;
+
+  const recurAddValue = (offset: number): void => {
+    if (offset === 0) {
+      return;
+    }
+
+    result += 1;
+    return recurAddValue(offset - 1);
+  };
+
+  recurAddValue(100);
+  recurAddValue(100);
+  recurAddValue(100);
+  recurAddValue(100);
+  recurAddValue(100);
+  recurAddValue(100);
+
+  return res.status(200).send(`${result}`);
 };
 
 export const health = (req: Request, res: Response) => {
